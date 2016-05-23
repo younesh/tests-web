c99badc7d41c5b4ab1bee43a30dd1c8e3d62e457
younesharke <younesharke@gmail.com>
2016-05-23 23:59:13 +0100

    # test angular suite : routing test

--- angularjs/app/routing.js
+++ angularjs/app/routing.js
@@ -2,3 +2,9 @@
  * Created by ychrak on 23/05/2016.
  */
 angular.module('Main', ['ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/m1',{ templateUrl:'partials/menu1.html'})
+            .when('/m2',{ templateUrl:'partials/menu2.html'})
+            .otherwise({redirectTo:'partials/p404.html'});
+    });
