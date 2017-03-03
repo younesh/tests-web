34a5599ee9fc1b0003097b1527dd7f7686957a45
ychrak <ychrak@webnet.fr>
2017-03-03 17:50:08 +0100

    suite  le test de mise en place d'un envir frontend avec brunch

--- theme-model/tpl-brunch-bootsr-sass/brunchWatch.bat
+++ theme-model/tpl-brunch-bootsr-sass/brunchWatch.bat
@@ -1,2 +1,45 @@
 @echo off
 start cmd.exe "brunch watch --server"
+
+---
+
+
+/* DECLARATION FUNCTION PART*/
+
+ function dispalyDatailUser () {
+
+ }
+
+
+// autenthif
+
+ Function autentifcation () {
+        var dfd = $.Deferred();
+        $.post(domainService + '/api/login_check',
+                {
+                    _username: NumDossier, // '4EABAA' ||  NumDossier
+                    _password: email    // 'benjamin@test.com' ||  email
+                }
+            ).done (function (data) { 
+	         var myToken = data.token;
+                 dfd.resolve(myToken);
+
+      	     })
+            .fail(function (data) {
+                dfd.reject();
+             });
+ }
+
+ Function autentifcation () {
+
+function getDetai
+
+
+  	     $.when(authentification).done(
+		 function() {
+                   $.when()
+		})
+
+
+
+
