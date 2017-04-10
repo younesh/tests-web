5f08bdfdab987d75f2cd67c8968ee1d38604094f
ychrak <ychrak@webnet.fr>
2017-04-10 11:31:47 +0200

    test

---
+++ theme-model/tpl-brunch-bootsrap/brunch-config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.config = {
+    files: {
+        javascripts: {
+            joinTo: {
+                'js/app.js': /^app\/js/,
+                'js/vendor.js': /^app\/vendor/
+            }
+        },
+        stylesheets: {
+            joinTo: {
+                'css/style.css': /^app\/scss/,
+                'css/vendor.css': /^app\/vendor/
+            }
+        }
+    },
+    conventions: {
+        assets: /(assets|pages)[\\/]/
+    },
+    plugins: {
+        sass: {
+            debug: 'comments'
+        }
+    },
+    watcher: {
+        awaitWriteFinish: true
+    }
+};
