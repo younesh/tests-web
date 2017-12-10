3c74b7148648ffafa4861a21dd58d7bceb452ad5
younes <younesharke@gmail.comgit config --global user.email younesharke@gmail.comgit config --global user.email younesharke@gmail.com>
2017-12-10 17:07:56 +0100

    test vueJS

---
+++ javascript/vueJS/compilass.bat
@@ -0,0 +1,4 @@
+D:
+cd D:\PROJETS\YC\WEB\tests-web\javascript\vueJS
+compass watch
+pause
---
+++ javascript/vueJS/js/vueApp.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#main',
+    data: {
+        message: "message hellow word vue JS !!! ",
+        link: 'http://yahoo.fr',
+        success: true, //  false | true
+        persons: [
+            {
+             nom : 'picatchu',
+             prenom : 'tatatsu'
+            },
+            {
+             nom : 'yahirou',
+             prenom : 'mamatsuru'
+            },
+            {
+             nom : 'haomaru',
+             prenom : 'takayuka'
+            },
+            {
+             nom : 'renju',
+             prenom : 'kensuka'
+            }
+        ]
+    }
+})
