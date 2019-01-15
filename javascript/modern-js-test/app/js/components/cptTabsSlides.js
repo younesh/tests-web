bed2c95dbcb037c2958dd181633bf32ebf761a29
Younes CHRAK <ychrak@webnet.local>
2019-01-15 18:04:59 +0100

    cpttabsSlide next

--- javascript/modern-js-test/app/js/components/cptTabsSlides.js
+++ javascript/modern-js-test/app/js/components/cptTabsSlides.js
@@ -1,9 +1,22 @@
 /** 
  * c'est un cpt qui soit sous forme de tabs soit de barres qui slide du contenu ...
  * cptTabsMenu
+ * @isTabsDesktop : boolean , il est a true par défault pour dir au cpt d'activer les tabs par defaut en desktop
  * 
 */
+
 export function initCPtTabsSlide ( isTabsDesktop = true) {
-    var H_item = $(".cpt-tabs-slides .tabs-item .item-ctn .ctn-wrapper").height();
+
+    const H_item = $(".cpt-tabs-slides .tabs-item .item-ctn .ctn-wrapper").height();
     $(".cpt-tabs-slides .tabs-item .item-ctn ").height(H_item);
+
+    $(".item-nav").click(handelClickitemNav);
+}
+
+/**
+ * fct : 
+*/
+
+function handelClickitemNav(evt) {
+    console.log(" hello handelClickitemNav ");
 } 
