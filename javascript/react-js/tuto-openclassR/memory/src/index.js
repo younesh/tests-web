865c851982258a8deddf44bbbc0bf6609e41b909
Younes CHRAK <ychrak@webnet.local>
2019-05-16 18:05:38 +0200

    react tuto openclassrom : ( wip )

---
+++ javascript/react-js/tuto-openclassR/memory/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+function CoolComponent({ attr1 = "attribute01" }) {
+  return <p>Youpi So Cool !!!!! hhhhh {attr1}</p>;
+}
+
+ReactDOM.render(
+  <CoolComponent attr1="newAtrrib01 ...... " />,
+  document.getElementById("root")
+);
