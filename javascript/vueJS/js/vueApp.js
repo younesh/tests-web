d8f2a465b90a8cb66092e3e2a58832e27f8f85fa
Younes CHRAK <ychrak@webnet.local>
2019-07-09 18:06:35 +0200

    vueJS officila: begining doc

--- javascript/vueJS/js/vueApp.js
+++ javascript/vueJS/js/vueApp.js
@@ -1,33 +1,33 @@
 new Vue({
-    el: '#main',
-    data: {
-        message: "message hellow word vue JS !!! ",
-        link: 'http://yahoo.fr',
-        title01: 'le titre du lien beuuuh ',
-        success: true, //  false | true
-        persons: [
-            {
-                nom: 'picatchu',
-                prenom: 'tatatsu'
-            },
-            {
-                nom: 'yahirou',
-                prenom: 'mamatsuru'
-            },
-            {
-                nom: 'haomaru',
-                prenom: 'takayuka'
-            },
-            {
-                nom: 'renju',
-                prenom: 'kensuka'
-            }
-        ]
-    }, 
-    methods: {
-        close: function () {
-            alert ("methode close applée @  !! ");
-            this.success = false; 
-        }
+  el: "#main",
+  data: {
+    message: "message vue js ... ",
+    link: "http://yahoo.fr",
+    title01: "le titre du lien beuuuh ",
+    success: true, //  false | true
+    persons: [
+      {
+        nom: "picatchu",
+        prenom: "tatatsu"
+      },
+      {
+        nom: "yahirou",
+        prenom: "mamatsuru"
+      },
+      {
+        nom: "haomaru",
+        prenom: "takayuka"
+      },
+      {
+        nom: "renju",
+        prenom: "kensuka"
+      }
+    ]
+  },
+  methods: {
+    close: function() {
+      alert("methode close applée @  !! ");
+      this.success = false;
     }
-})
+  }
+});
