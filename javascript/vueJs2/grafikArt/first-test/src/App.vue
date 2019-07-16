0fd966baaffe6970c0bd18fce5e044ab2533ef81
Younes CHRAK <ychrak@webnet.local>
2019-07-16 17:08:06 +0200

    VueJs : revision , cours, test ...

---
+++ javascript/vueJs2/grafikArt/first-test/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <cptTest title="my cpt custom title lol !!! " />3
+    <br />----------- app ctn
+    <br />
+    <h2>title in app</h2>
+    <br />
+  </div>
+</template>
+
+<script>
+import cptTest from "./components/cptTest.vue";
+
+export default {
+  name: "app",
+  components: {
+    //HelloWorld,
+    cptTest
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ javascript/vueJs2/grafikArt/first-test/src/components/cptTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cpt-test">
+    <h2>title = {{title}}</h2>
+    <button @click="clicTest">click test evant</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cptTest",
+  props: {
+    title: String
+  },
+  methods: {
+    clicTest: function() {
+      console.log("cptTest ....  ");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only  :  -->
+<style scoped>
+h2 {
+  color: green;
+}
+</style>
