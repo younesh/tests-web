eda458eeff5ac2c4ddc003f17052bedae1f62f46
younes <younesharke@gmail.comgit config --global user.email younesharke@gmail.comgit config --global user.email younesharke@gmail.com>
2019-07-28 17:25:46 +0200

    vue js : test ...

---
+++ javascript/vueJS/grafikArt-test/js/vueApp.js
@@ -0,0 +1,33 @@
+new Vue({
+    el: '#main',
+    data: {
+        message: "message hellow word vue JS !!! ",
+        link: 'http://yahoo.fr',
+        title01: 'le titre du lien beuuuh ',
+        success: true, //  false | true
+        persons: [
+            {
+                nom: 'picatchu',
+                prenom: 'tatatsu'
+            },
+            {
+                nom: 'yahirou',
+                prenom: 'mamatsuru'
+            },
+            {
+                nom: 'haomaru',
+                prenom: 'takayuka'
+            },
+            {
+                nom: 'renju',
+                prenom: 'kensuka'
+            }
+        ]
+    }, 
+    methods: {
+        close: function () {
+            alert ("methode close applée @  !! ");
+            this.success = false; 
+        }
+    }
+})
