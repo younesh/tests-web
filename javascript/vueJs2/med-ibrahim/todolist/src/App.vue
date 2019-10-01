3edb8809df27bbee4a4d3cd897fc04eda2d9cd6b
younes <younesharke@gmail.comgit config --global user.email younesharke@gmail.comgit config --global user.email younesharke@gmail.com>
2019-10-01 12:37:48 +0200

    vue js : nexte dev

--- javascript/vueJs2/med-ibrahim/todolist/src/App.vue
+++ javascript/vueJs2/med-ibrahim/todolist/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,13 +9,8 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+export default {
+  
 }
-</style>
+</script>
--- javascript/vueJs2/med-ibrahim/todolist/src/router.js
+++ javascript/vueJs2/med-ibrahim/todolist/src/router.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Todos from './components/Todos.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'todos',
+            component: Todos
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        }
+    ]
 })
