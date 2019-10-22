dadcdcda5d4c0cd7d47b7be6830612de5c989c2f
younes <younesharke@gmail.comgit config --global user.email younesharke@gmail.comgit config --global user.email younesharke@gmail.com>
2019-10-22 12:31:41 +0200

    todo-03 : STEP > add a new todo

--- javascript/vueJs2/apps-todo/todo-03-idbrahim/src/components/Todos.vue
+++ javascript/vueJs2/apps-todo/todo-03-idbrahim/src/components/Todos.vue
@@ -3,9 +3,9 @@
   <h1>list OPcvm test  </h1>
   <form action="">
       <div class="from-group">
-           <input v-model="title" type="text" class="form-control" placeholder="new todo ...">
+           <input v-model="currentTitle" type="text" class="form-control" placeholder="new todo ...">
       </div>
-      <button @click="addTodo()" class="btn btn-primary btn-block btn-success mb-2 mt-2">add todo </button>
+      <button @click.prevent="addTodo()" class="btn btn-primary btn-block btn-success mb-2 mt-2">add todo </button>
   </form>
   <ul class="list-group">
       <li class="list-group-item" v-bind:key="todo.id" v-for="todo in todos">
@@ -27,19 +27,35 @@ export default {
     },
     data() {
         return {
-            title: '',
+            currentTitle: '', // le titre saisie ds le champ de text du from
             todos : [ ]
         }
     },
     methods : {
         addTodo () {
-            axios.post("http://localhost:5000/todos/")
-            //   .then(res => console.log(res))
+            if (this.currentTitle !== '') {
+                let newTodo = { // objet todo temporaire pour hydrater la BD
+
+                    title : this.currentTitle,
+                    completed : false
+                } 
+                // ajoutant le nouveau todo a la BD
+                axios.post("http://localhost:5000/todos/", newTodo)
+                .then(res => {
+                    // et puis en ajout a la list en front ( this.todos ) le nouveau todo ajouté ds la BD
+                    this.todos = [res.data, ...this.todos]
+
+                    // puis apres l'ajout en vide les champs
+                   this.currentTitle = '';
+
+                })
+            }
+
         },
         getTodos() {
              axios.get("http://localhost:5000/todos")
                 .then(res => this.todos = res.data)
-                // .catch(err => console.log(err))
+                .catch(err => console.log("ERR object : ",err))
         },
         deleteOneTodo(id) {
             if (confirm("are you shure to delete this item ? ")){
